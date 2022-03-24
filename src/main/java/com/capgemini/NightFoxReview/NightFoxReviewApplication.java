2a8ab3a94588c69fa6df59d11c033ed33a4fca8b
qixiaoling <x.qi@novi-education.nl>
2022-03-24 15:14:41 +0100

    second commit

--- src/main/java/com/capgemini/NightFoxReview/NightFoxReviewApplication.java
+++ src/main/java/com/capgemini/NightFoxReview/NightFoxReviewApplication.java
@@ -1,21 +1,33 @@
 package com.capgemini.NightFoxReview;
 
+import com.capgemini.NightFoxReview.model.Review;
+import com.capgemini.NightFoxReview.service.ReviewService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class NightFoxReviewApplication {
-
+public class NightFoxReviewApplication implements CommandLineRunner {
+	@Autowired
+	ReviewService reviewService;
 	public static void main(String[] args) {
+
 		SpringApplication.run(NightFoxReviewApplication.class, args);
+
+
+
 	}
 
-	@GetMapping
-	public String hello(){
-		return "I am from test";
-	}
 
+
+	@Override
+	public void run(String... args) throws Exception {
+		reviewService.addReviewByArtistId(1L, new Review("Micky", "Good job!", true, 1L));
+
+	}
 }
--- src/main/java/com/capgemini/NightFoxReview/service/ReviewService.java
+++ src/main/java/com/capgemini/NightFoxReview/service/ReviewService.java
@@ -3,6 +3,7 @@ package com.capgemini.NightFoxReview.service;
 import com.capgemini.NightFoxReview.Exception.BadRequestException;
 import com.capgemini.NightFoxReview.Exception.GlobalExceptionHandler;
 import com.capgemini.NightFoxReview.Exception.NotFoundException;
+import com.capgemini.NightFoxReview.model.Artist;
 import com.capgemini.NightFoxReview.model.Review;
 import com.capgemini.NightFoxReview.repository.ReviewRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -51,20 +52,36 @@ public class ReviewService {
         return reviewRepository.findByArtistId(id);
     }
 
-    public void addReviewByArtistId(Long artistId, Review review){
-      webClientBuilder.build()
+    public ResponseEntity<?> addReviewByArtistId(Long artistId, Review review){
+      Artist artist = webClientBuilder.build()
                     .get()
-                    .uri("http://localhost:8082/artist/getbyid" + artistId)
+                    .uri("http://localhost:8080/artist/getbyid/" + artistId)
                     .retrieve()
-                .onStatus(
+              .onStatus(
                         HttpStatus.INTERNAL_SERVER_ERROR::equals,
                         clientResponse -> clientResponse.bodyToMono(String.class).map(Exception::new))
-                .onStatus(
+              .onStatus(
                         HttpStatus.BAD_REQUEST::equals,
                         clientResponse -> clientResponse.bodyToMono(String.class).map(BadRequestException::new))
                 .onStatus(
                         HttpStatus.NOT_FOUND::equals,
-                        clientResponse -> clientResponse.bodyToMono(String.class).map(NotFoundException::new));
+                        clientResponse -> clientResponse.bodyToMono(String.class).map(NotFoundException::new))
+              .bodyToMono(Artist.class)
+              .doOnSuccess(s -> {
+                  reviewRepository.save(review);
+              })
+//              .subscribe(System.out::println);
+              .block();
+      return ResponseEntity.ok().body("Your review on "+ artist.getBandName() + " is made.");
+
+
+
+
+
+
+
+//              .toEntity(HttpStatus.class)
+//              .map(response -> response.getStatusCode());
 
 
 
